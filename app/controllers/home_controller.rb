d309fd2556c3e9c797fe2fcf94fa3fc4b675ac18
Marnie L. Paraiso <marnie.paraiso@paramount.com.ph>
2016-03-22 15:50:25 +0800

    add pages - [short codes, about, contact]

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
   def index
   end
+  
+  def about
+  end
+  
+  def contact
+  end
+  
+  def short_codes
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get "/short-codes" => "home#short_codes"
+  get "/contact" => "home#contact"
+  get "/about" => "home#about"
   root "home#index"
 end
